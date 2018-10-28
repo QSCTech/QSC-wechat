d6de7ee70d9ca2669a468d5a5210ebe0d877a756
laphets <i@laphets.com>
2018-10-28 10:49:44 +0800

    Build up auth redirect

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,7 @@
-const prefix_dev = 'http://localhost:3000'
-const prefix_prod = 'https://www.qsc.zju.edu.cn'
+const prefix = {
+    dev: 'http://localhost:8080',
+    prod: 'https://www.qsc.zju.edu.cn'
+}
 
 import * as storage from './storage'
 
@@ -7,7 +9,7 @@ export const http = ({ method, url, data }) => {
     return new Promise((resolve, reject) => {
         method = method.toUpperCase()
         wx.request({
-            url,
+            url: prefix.dev + url,
             method,
             header: {
                 'Authorization': `Bearer ${storage.get("token")}` || ''
@@ -15,15 +17,15 @@ export const http = ({ method, url, data }) => {
             data,
             dataType: 'json',
             success: (res) => {
-                try {
-                    const json = JSON.parse(res.data)
-                    res.data = json
-                    resolve(res)
-                } catch (error) {
-                    resolve(res)
+                if (res.data.code !== 0) {
+                    reject(res.data)
+                } else {
+                    resolve(res.data)  
                 }
             },
-            fail: (err) => { reject(err) }
+            fail: (err) => {
+                reject({ code: -1, message: "wepy error", data: err })
+            }
         })
     })
 }
