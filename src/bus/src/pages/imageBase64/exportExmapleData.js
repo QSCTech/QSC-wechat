c2023e05c52f31fb4e45c583bc4ba16517555dde
Abracax <AbraxasAmI@gmail.com>
2018-11-04 21:47:00 +0800

    a

---
+++ src/bus/src/pages/imageBase64/exportExmapleData.js
@@ -0,0 +1,26 @@
+let data = {
+  'errmsg': '',
+  'total': 4,
+  'rows': [{'classid': 3036,
+    'stime': '07: 30',
+    'etime': '09: 00',
+    'ptime': '08: 30',
+    'ssite': '海宁国际校区',
+    'esite': '紫金港校区',
+    'psite': '-',
+    'busnum': 6,
+    'booknum': 0
+  },
+  {
+    'classid': 3041,
+    'stime': '05: 30',
+    'etime': '02: 00',
+    'ptime': '-',
+    'ssite': '紫金港校区',
+    'esite': '海宁国际校区',
+    'psite': '玉泉校区',
+    'busnum': 40,
+    'booknum': 23
+  }]}
+
+module.exports = {TestMessage: data}
